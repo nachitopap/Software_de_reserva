ec64ed0bd0d23a1c6f06e6f778461a1888c3e2b4
nachitopap <ignacioabarca77@gmail.com>
2026-08-23 00:45:01 -0400

    cambios en navbar.php estaba mal la URL del require

--- app/views/layouts/navbar.php
+++ app/views/layouts/navbar.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../config/config.php';
+require_once __DIR__ . '/../../../config/config.php';
 $role = $_SESSION['user_role'] ?? '';
 $name = $_SESSION['user_name'] ?? 'Usuario';
 
